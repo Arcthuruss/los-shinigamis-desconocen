db824a8d3ba08b29c73ce78abe96cd85bb73d646
PIlouFace <24657574+Pilou26021@users.noreply.github.com>
2025-12-01 16:22:18 +0100

    ogugif favicon

--- dashboard/src/main.tsx
+++ dashboard/src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import oguGif from './assets/ogu.gif'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <link rel="icon" href={oguGif} type="image/gif" />
     <App />
   </StrictMode>,
 )
